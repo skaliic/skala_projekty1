b8ffd93de4ac14e51f00fcb486ae69eae7d09ad8
Jakub Skála <skalaj24@spstrutnov.cz>
2025-09-17 11:12:14 +0200

    kamen-nuzky-papir

--- Mini_Projekt/Mini_Projekt/Program.cs
+++ Mini_Projekt/Mini_Projekt/Program.cs
@@ -1,9 +1,38 @@
 ﻿using system;
 
+using System;
+
 class Program
 {
-    static void main()
+    static void Main()
     {
         string[] moznosti = { "kámen", "nůžky", "papír" };
         Random rnd = new Random();
+
+        Console.Write("Zadej kámen, nůžky nebo papír: ");
+        string hrac = Console.ReadLine().ToLower();
+
+        string pocitac = moznosti[rnd.Next(moznosti.Length)];
+        Console.WriteLine($"Počítač zvolil: {pocitac}");
+
+        if (hrac == pocitac)
+        {
+            Console.WriteLine("Remíza!");
+        }
+        else if ((hrac == "kámen" && pocitac == "nůžky") ||
+                 (hrac == "nůžky" && pocitac == "papír") ||
+                 (hrac == "papír" && pocitac == "kámen"))
+        {
+            Console.WriteLine("Vyhrál jsi!");
+        }
+        else
+        {
+            Console.WriteLine("Prohrál jsi!");
+        }
+
+        Console.WriteLine("Stiskni libovolnou klávesu pro ukončení...");
+        Console.ReadKey();
+    }
+}
+
 }
